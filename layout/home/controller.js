7b9ae3e7a87e9cdf999a2c48a4838303d419612e
Prio Dwi Sembodo <49172376+priooods@users.noreply.github.com>
2023-12-31 02:32:58 +0700

    FINISH

--- layout/home/controller.js
+++ layout/home/controller.js
@@ -3,12 +3,24 @@ $(document).ready(function () {
   const prev = document.getElementById("prev");
   const swiper = new Swiper(".swiper", {
     speed: 400,
-    slidesPerView: 3,
-    spaceBetween: -300,
     autoplay: false,
     a11y: {
       prevSlideMessage: "Previous slide",
       nextSlideMessage: "Next slide",
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: -50,
+      },
+      480: {
+        slidesPerView: 2,
+        spaceBetween: -50,
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
     },
   });
   next.addEventListener("click", function () {
